45f1820e76bf32a720d7a275b545c8f1dee5f565
Omar Alcarraz <omaralcarraz@gmail.com>
2026-03-11 17:29:51 -0300

    version parcial de la trivia v4

--- trivia.py
+++ trivia.py
@@ -1,25 +1,54 @@
-nombre=input("introduce tu nombre")
+nombre=input("introduce tu nombre: ")
 bienvenida=print(f"Hola {nombre}, bienvenido a la trivia.")
 instrucciones = print("responde correctamente las pregunetas para ganar puntos.")
 puntos = 0
 
-pregunta1 = input("cuantos angulos tiene un triangulo?")
+pregunta1 = input("cuantos angulos tiene un triangulo?: ")
 respuesta_correcta1 = 3
+
 if pregunta1 == respuesta_correcta1:
+    puntos+=1    
     print("respuesta correcta!!")
-    puntos+=1
+    
 else:
     print(f"respuesta incorrecta, la respuesta era {respuesta_correcta1}")
     puntos=puntos
 print(f"tus puntos acumulados son {puntos}")
 
-pregunta2 = input("cuantos dias tiene una semana")
+pregunta2 = input("cuantos dias tiene una semana?: ")
 respuesta_correcta2 = 7
+
 if pregunta2 == respuesta_correcta2:
-    print("respuesta correcta!!")
     puntos+=1
+    print("respuesta correcta!!")
+    
 else:
     print(f"respuesta incorrecta, la respuesta era {respuesta_correcta2}")
     puntos=puntos
+
 print(f"tus puntos acumulados son {puntos}")
 
+pregunta3 = input(" ")
+respuesta_correcta3 = 
+
+if pregunta3 == respuesta_correcta3:
+    puntos+=1    
+    print("respuesta correcta!!")
+    
+else:
+    print(f"respuesta incorrecta, la respuesta era {respuesta_correcta3}")
+    puntos=puntos
+print(f"tus puntos acumulados son {puntos}")
+
+pregunta4 = input(" ")
+respuesta_correcta4 = 
+
+if pregunta4 == respuesta_correcta4:
+    puntos+=1
+    print("respuesta correcta!!")
+    
+else:
+    print(f"respuesta incorrecta, la respuesta era {respuesta_correcta4}")
+    puntos=puntos
+
+print(f"tus puntos acumulados son {puntos}")
