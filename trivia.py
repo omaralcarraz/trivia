3fcf1153478036f1af8797f432ba22b533626016
Omar Alcarraz <omaralcarraz@gmail.com>
2026-03-12 10:21:43 -0300

    version fina l de la trivia

--- trivia.py
+++ trivia.py
@@ -1,13 +1,13 @@
 nombre=input("introduce tu nombre: ")
 bienvenida=print(f"Hola {nombre}, bienvenido a la trivia.")
-instrucciones = print("responde correctamente las pregunetas para ganar puntos.")
+instrucciones = print("responde correctamente las preguntas para ganar puntos.")
 puntos = 0
 
-pregunta1 = input("cuantos angulos tiene un triangulo?: ")
+pregunta1 = int(input("cuantos angulos tiene un triangulo?: "))
 respuesta_correcta1 = 3
 
 if pregunta1 == respuesta_correcta1:
-    puntos+=1    
+    puntos=puntos+1    
     print("respuesta correcta!!")
     
 else:
@@ -15,11 +15,11 @@ else:
     puntos=puntos
 print(f"tus puntos acumulados son {puntos}")
 
-pregunta2 = input("cuantos dias tiene una semana?: ")
+pregunta2 = int(input("cuantos dias tiene una semana?: "))
 respuesta_correcta2 = 7
 
 if pregunta2 == respuesta_correcta2:
-    puntos+=1
+    puntos=puntos+1
     print("respuesta correcta!!")
     
 else:
@@ -28,11 +28,11 @@ else:
 
 print(f"tus puntos acumulados son {puntos}")
 
-pregunta3 = input(" ")
-respuesta_correcta3 = 
+pregunta3 = int(input("cuantas vocales hay en la palabra 'muricelago'?: "))
+respuesta_correcta3 = 5
 
 if pregunta3 == respuesta_correcta3:
-    puntos+=1    
+    puntos=puntos+1    
     print("respuesta correcta!!")
     
 else:
@@ -40,15 +40,15 @@ else:
     puntos=puntos
 print(f"tus puntos acumulados son {puntos}")
 
-pregunta4 = input(" ")
-respuesta_correcta4 = 
+pregunta4 = int(input("cuanto es 48-13?: "))
+respuesta_correcta4 = 35
 
 if pregunta4 == respuesta_correcta4:
-    puntos+=1
+    puntos=puntos+1
     print("respuesta correcta!!")
     
 else:
     print(f"respuesta incorrecta, la respuesta era {respuesta_correcta4}")
     puntos=puntos
 
-print(f"tus puntos acumulados son {puntos}")
+print(f"conseguiste un total de {puntos} puntos")
